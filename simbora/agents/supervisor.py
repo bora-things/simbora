4506436b73858376cc0f44c0ab16ca6484642082
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2025-06-13 14:40:19 -0300

    fix: update model to gpt-4o-mini and refine prompt descriptions in supervisor

--- simbora/agents/supervisor.py
+++ simbora/agents/supervisor.py
@@ -4,7 +4,7 @@ from simbora.agents.sigaa import sigaa_agent
 from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(
-    model="gpt-4o",
+    model="gpt-4o-mini",
     temperature=0,
     max_tokens=None,
     timeout=None,
@@ -17,8 +17,8 @@ supervisor = create_supervisor(
     agents=[rag_agent, sigaa_agent],
     prompt=(
         "Você é um supervisor que gerencia dois agentes:\n"
-        "- um agente de RAG. Atribua tarefas relacionadas à dúvidas gerais da UFRN (como PPCs de cursos, FAQs, regulamentos) para este agente\n"
-        "- um agente consumidor da API do SIGAA. Atribua tarefas relacionadas à informações dinâmicas da UFRN como notícias, calendários acadêmicos, avaliações de docentes, componentes curriculares para este agente\n"
+        "- um agente de RAG. Atribua tarefas relacionadas à dúvidas relacionadas à PPCs de cursos, FAQs e regulamentos da UFRN para este agente\n"
+        "- um agente consumidor da API do SIGAA. Atribua tarefas relacionadas à informações dinâmicas da UFRN como notícias, calendários acadêmicos, avaliações de docentes, componentes curriculares (disciplinas) para este agente\n"
         "Não execute nenhum trabalho você mesmo."
         "A resposta final deve ser uma resposta completa e concisa, que responda à pergunta do usuário."
     )
